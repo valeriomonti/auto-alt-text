4b2278564d0ea19ae8722d610392c676747ef357
Valerio Monti <valerio.monti@vmweb.it>
2023-05-27 16:25:00 +0200

    feat: use openai-php/client package to implement api calls

--- src/App/AltTextGeneratorAi.php
+++ src/App/AltTextGeneratorAi.php
@@ -3,6 +3,7 @@
 namespace ValerioMonti\AutoAltText\App;
 
 use Composer\Installers\Plugin;
+use OpenAI;
 use ValerioMonti\AutoAltText\App\AltTextGeneratorInterface;
 use ValerioMonti\AutoAltText\App\Admin\PluginOptions;
 
@@ -10,40 +11,17 @@ class AltTextGeneratorAi implements AltTextGeneratorInterface
 {
     public function altText(int $imageId): string
     {
-        $altText = '';
         $imageUrl = wp_get_attachment_url($imageId);
-        $url = 'https://api.openai.com/v1/chat/completions';
         $apiKey = PluginOptions::apiKey();
 
-        $args = array(
-            'method' => 'POST',
-            'headers' => array(
-                'Authorization' => 'Bearer ' . $apiKey,
-                'Content-Type' => 'application/json',
-            ),
-            'body' => json_encode(array(
-                'model' => 'gpt-3.5-turbo',
-                'messages' => [
-                    [
-                        "role" => "user",
-                        "content" => "Agisci come un esperto SEO e scrivi un alt text in italiano lungo al massimo 15 parole per questa immagine: " . $imageUrl,
-                    ]
-                ],
-                'max_tokens' => 100,
-                'temperature' => 1,
-            ))
-        );
+        $client = OpenAI::client($apiKey);
 
-        $response = wp_remote_post($url, $args);
+        $result = $client->completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => "Agisci come un esperto SEO e scrivi un alt text in italiano lungo al massimo 15 parole per questa immagine: " . $imageUrl . ". Limitati semplicemente a ritornare il testo senza virgolette.",
+            'temperature' => 1
+        ]);
 
-        if (is_wp_error($response)) {
-            $errorMessage = $response->get_error_message();
-            echo "Something went wrong: $errorMessage";
-        } else {
-            $data = json_decode(wp_remote_retrieve_body($response), true);
-            $altText = $data['choices'][0]['message']['content'] ?? '';
-        }
-
-        return $altText;
+        return trim($result['choices'][0]['text']);
     }
 }
