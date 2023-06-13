37f2a46ba6546443ef63ffed0a4753328ef520a7
Valerio Monti <valerio.monti@vmweb.it>
2023-06-13 14:48:52 +0200

    chore: send the imageUrl via api request

--- src/App/AIProviders/Azure/AzureComputerVisionCaptionsResponse.php
+++ src/App/AIProviders/Azure/AzureComputerVisionCaptionsResponse.php
@@ -16,13 +16,15 @@ class AzureComputerVisionCaptionsResponse implements AIProviderInterface
                     'Ocp-Apim-Subscription-Key'     => PluginOptions::apiKeyAzure(),
                 ],
                 'body' => json_encode([
-                    'url' => 'https://images.unsplash.com/photo-1668554245790-bfdc72f0bb3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+                    'url' => $imageUrl,
+                    //Dev example 'https://images.unsplash.com/photo-1668554245790-bfdc72f0bb3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
                 ]),
                 'method' => 'POST',
             ]
         );
 
         $bodyResult = json_decode(wp_remote_retrieve_body($response), true);
+        //@TODO manage errors
         return $bodyResult['captionResult']['text'];
     }
 }
