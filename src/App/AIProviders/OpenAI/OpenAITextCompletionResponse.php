d62b67b94b4dc4206fad06c4803ea46e8879ec2d
Valerio Monti <valerio.monti@vmweb.it>
2023-10-17 23:32:00 +0200

    fix: fix the OpenAi response

--- src/App/AIProviders/OpenAI/OpenAITextCompletionResponse.php
+++ src/App/AIProviders/OpenAI/OpenAITextCompletionResponse.php
@@ -4,6 +4,7 @@ namespace ValerioMonti\AutoAltText\App\AIProviders\OpenAI;
 use OpenAI;
 use OpenAI\Client;
 use ValerioMonti\AutoAltText\App\Admin\PluginOptions;
+use ValerioMonti\AutoAltText\Config\Constants;
 
 class OpenAITextCompletionResponse extends OpenAIResponse
 {
@@ -17,7 +18,6 @@ class OpenAITextCompletionResponse extends OpenAIResponse
     }
     public function response(string $imageUrl): string
     {
-
         $model = PluginOptions::model();
         $apiKey = PluginOptions::apiKeyOpenAI();
         $prompt = parent::prompt($imageUrl);
@@ -26,9 +26,16 @@ class OpenAITextCompletionResponse extends OpenAIResponse
         $result = $client->completions()->create([
             'model' => $model,
             'prompt' => $prompt,
+            'max_tokens' => Constants::AAT_OPENAI_MAX_TOKENS,
             'temperature' => 1
         ]);
 
-        return trim(preg_replace('/\s\s+\"/', '', $result['choices'][0]['text']));
+        $patterns = array(
+            '/\"/',        // Double quotes
+            '/\s\s+/',     // Double or more consecutive white spaces
+            '/&quot;/'     // HTML sequence for double quotes
+        );
+
+        return trim(preg_replace($patterns, '', $result['choices'][0]['text']));
     }
 }
