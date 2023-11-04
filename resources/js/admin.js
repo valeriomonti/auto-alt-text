396ce5be1d8da9a1cc202dd5d82fd604d9d5f5f9
Valerio Monti <valerio.monti@vmweb.it>
2023-11-04 15:24:57 +0100

    fix: verify dom is loaded before filtering in options page

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,34 +1,37 @@
-const typologyField = document.getElementById('aat_typology');
-const azureLanguageField = document.getElementById('aat-language-azure-translate-instance');
+document.addEventListener('DOMContentLoaded', function() {
 
-const filterFields = (typology, language) => {
-    let optionFields = document.querySelectorAll('.plugin-option');
+    const typologyField = document.getElementById('aat_typology');
+    const azureLanguageField = document.getElementById('aat-language-azure-translate-instance');
+    const filterFields = (typology, language) => {
+        let optionFields = document.querySelectorAll('.plugin-option');
 
-    optionFields.forEach((pluginOption) => {
-        let inputs = pluginOption.querySelectorAll('input, select, textarea');
-        if (pluginOption.classList.contains('type-' + typology)) {
-            if (typology !== 'azure' || language !== 'en' || !pluginOption.classList.contains('not-default-language')) {
-                pluginOption.style.display = 'block';
-                inputs.forEach(input => input.setAttribute('required', ''));
+        optionFields.forEach((pluginOption) => {
+            let inputs = pluginOption.querySelectorAll('input, select, textarea');
+            if (pluginOption.classList.contains('type-' + typology)) {
+                if (typology !== 'azure' || language !== 'en' || !pluginOption.classList.contains('not-default-language')) {
+                    pluginOption.style.display = 'block';
+                    inputs.forEach(input => input.setAttribute('required', ''));
+                } else {
+                    pluginOption.style.display = 'none';
+                    inputs.forEach(input => input.removeAttribute('required'));
+                }
             } else {
                 pluginOption.style.display = 'none';
                 inputs.forEach(input => input.removeAttribute('required'));
             }
-        } else {
-            pluginOption.style.display = 'none';
-            inputs.forEach(input => input.removeAttribute('required'));
-        }
+        });
+    };
+
+    filterFields(typologyField.value, azureLanguageField.value);
+
+    typologyField.addEventListener('change', function (event) {
+        let typology = event.target.value;
+        filterFields(typology, azureLanguageField.value);
     });
-};
 
-filterFields(typologyField.value, azureLanguageField.value);
+    azureLanguageField.addEventListener('change', function (event) {
+        let language = event.target.value;
+        filterFields(typologyField.value, language);
+    });
 
-typologyField.addEventListener('change', function (event) {
-    let typology = event.target.value;
-    filterFields(typology, azureLanguageField.value);
-});
-
-azureLanguageField.addEventListener('change', function (event) {
-    let language = event.target.value;
-    filterFields(typologyField.value, language);
 });
