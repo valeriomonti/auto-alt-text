a55523cd9be4fed8ff8cd69a0b57577bb8e57056
Valerio Monti <valerio.monti@vmweb.it>
2024-08-20 14:26:50 +0200

    feat: enable alt text generation button in single attachment page

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -9,6 +9,7 @@ class Constants
     const AATXT_PLUGIN_OPTIONS_PAGE_SLUG = 'auto-alt-text-options';
     const AATXT_PLUGIN_OPTION_LOG_PAGE_SLUG = 'auto-alt-text-log';
     const AATXT_PLUGIN_ASSETS_HANDLE = 'aatxt-auto-alt-text-options';
+    const AATXT_PLUGIN_MEDIA_LIBRARY_HANDLE = 'aatxt-auto-alt-text-media-library';
     const AATXT_OPTION_FIELD_TYPOLOGY = 'aatxt_typology';
     const AATXT_OPTION_FIELD_PROMPT_OPENAI = 'aatxt_prompt_openai';
     const AATXT_OPTION_FIELD_API_KEY_OPENAI = 'aatxt_api_key_openai';
